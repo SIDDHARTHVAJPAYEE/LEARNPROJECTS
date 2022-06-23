b10242e07b8d8a53274250eaeeaf9a460d3bef0f
Siddharth <siddharthvajpayee92@gmail.com>
2022-06-23 16:21:33 +0530

    TYPECASTING

---
+++ TypeCasting.java
@@ -0,0 +1,16 @@
+package LEARNPROJECT;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		System.out.println("Implicit Type Casting");
+		char a='A';
+		System.out.println("Char type: "+a);
+		int x=a;
+		System.out.println("Int type : "+x);
+		float y=x;
+		System.out.println("Float value is : "+y);
+		double b=y;
+		System.out.println("Double Value is : "+b);
+	}
+
+}
