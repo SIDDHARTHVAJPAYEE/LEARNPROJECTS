7ad3ef9d633071a686efc2c697040abb7d5bfb75
Siddharth <siddharthvajpayee92@gmail.com>
2022-06-24 11:16:43 +0530

    Collections

---
+++ collectionAssisted.java
@@ -0,0 +1,26 @@
+package LEARNPROJECT;
+import java.util.*;
+public class collectionAssisted {
+
+	public static void main(String[] args) {
+		System.out.println("ArrayList");
+		ArrayList<String> city=new ArrayList<>();
+		city.add("BANGALORE");
+		city.add("Gwalior");
+		city.add("Indore");
+		System.out.println(city);
+		Integer arr[]=new Integer[20];
+		arr[0]=50;
+		Vector<Integer> number=new Vector<>();
+		number.add(89);
+		number.add(74);
+		number.add(12);
+		System.out.println(number);
+		HashSet<String> friends=new HashSet<>();
+		friends.add("Rahul");
+		friends.add("Gaurang");
+		friends.add("ayush");
+		System.out.println(friends);
+	}
+
+}
